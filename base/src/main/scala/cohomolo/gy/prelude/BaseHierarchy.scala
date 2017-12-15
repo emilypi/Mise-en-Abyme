3f40c3939bb6055cbece724c3b4f0e51589bc520
Emily Pillmore <emilyp@cakesolutions.net>
2017-12-15 13:10:57 -0500

    fmt

--- base/src/main/scala/cohomolo/gy/prelude/BaseHierarchy.scala
+++ base/src/main/scala/cohomolo/gy/prelude/BaseHierarchy.scala
@@ -7,24 +7,33 @@ class BaseHierarchy extends BaseHierarchy.BH0
 object BaseHierarchy {
   trait BH0 extends BH1 {
     implicit def monadBind[M[_]](implicit M: Monad[M]): Bind[M] = M.bind
-    implicit def monadApplicative[M[_]](implicit M: Monad[M]): Applicative[M] = M.applicative
-    implicit def monadApply[M[_]](implicit M: Monad[M]): Apply[M] = M.applicative.apply
-    implicit def monadFunctor[M[_]](implicit M: Monad[M]): Functor[M] = M.applicative.apply.functor
-    implicit def monoidSemigroup[A](implicit A: Monoid[A]): Semigroup[A] = A.semigroup
-    implicit def comonadCobind[F[_]](implicit F: Comonad[F]): Cobind[F] = F.cobind
+    implicit def monadApplicative[M[_]](implicit M: Monad[M]): Applicative[M] =
+      M.applicative
+    implicit def monadApply[M[_]](implicit M: Monad[M]): Apply[M] =
+      M.applicative.apply
+    implicit def monadFunctor[M[_]](implicit M: Monad[M]): Functor[M] =
+      M.applicative.apply.functor
+    implicit def monoidSemigroup[A](implicit A: Monoid[A]): Semigroup[A] =
+      A.semigroup
+    implicit def comonadCobind[F[_]](implicit F: Comonad[F]): Cobind[F] =
+      F.cobind
   }
 
   trait BH1 extends BH2 {
     implicit def bindApply[M[_]](implicit M: Bind[M]): Apply[M] = M.apply
-    implicit def bindFunctor[M[_]](implicit M: Bind[M]): Functor[M] = M.apply.functor
+    implicit def bindFunctor[M[_]](implicit M: Bind[M]): Functor[M] =
+      M.apply.functor
   }
 
   trait BH2 extends BH3 {
-    implicit def applicativeApply[M[_]](implicit M: Applicative[M]): Apply[M] = M.apply
-    implicit def applicativeFunctor[M[_]](implicit M: Applicative[M]): Functor[M] = M.apply.functor
+    implicit def applicativeApply[M[_]](implicit M: Applicative[M]): Apply[M] =
+      M.apply
+    implicit def applicativeFunctor[M[_]](
+        implicit M: Applicative[M]): Functor[M] = M.apply.functor
   }
 
   trait BH3 {
-    implicit def applyFunctor[M[_]](implicit M: Apply[M]): Functor[M] = M.functor
+    implicit def applyFunctor[M[_]](implicit M: Apply[M]): Functor[M] =
+      M.functor
   }
 }
--- base/src/main/scala/cohomolo/gy/prelude/Prelude.scala
+++ base/src/main/scala/cohomolo/gy/prelude/Prelude.scala
@@ -5,18 +5,17 @@ import cohomolo.gy.prelude.syntax._
 
 import scala.language.implicitConversions
 
-
-trait Prelude  extends
-  prelude.functions.BindFunctions
-  with prelude.functions.FunctorFunctions
-  with prelude.functions.CobindFunctions {
+trait Prelude
+    extends prelude.functions.BindFunctions
+    with prelude.functions.FunctorFunctions
+    with prelude.functions.CobindFunctions {
 
   // Base Class
   // ==========
   type Applicative[F[_]] = typeclasses.Applicative[F]
   type Apply[F[_]] = typeclasses.Apply[F]
   type Bind[M[_]] = typeclasses.Bind[M]
-  type Compose[P[_,_]] = typeclasses.Compose[P]
+  type Compose[P[_, _]] = typeclasses.Compose[P]
   type Functor[F[_]] = typeclasses.Functor[F]
   type Monad[M[_]] = typeclasses.Monad[M]
   type Comonad[F[_]] = typeclasses.Comonad[F]
@@ -28,7 +27,7 @@ trait Prelude  extends
   def Applicative[F[_]](implicit F: Applicative[F]): Applicative[F] = F
   def Apply[F[_]](implicit F: Apply[F]): Apply[F] = F
   def Bind[F[_]](implicit F: Bind[F]): Bind[F] = F
-  def Compose[P[_,_]](implicit P: Compose[P]): Compose[P] = P
+  def Compose[P[_, _]](implicit P: Compose[P]): Compose[P] = P
   def Functor[F[_]](implicit F: Functor[F]): Functor[F] = F
   def Monad[M[_]](implicit M: Monad[M]): Monad[M] = M
   def Comonad[F[_]](implicit F: Comonad[F]): Comonad[F] = F
@@ -38,37 +37,43 @@ trait Prelude  extends
   def Monoid[T](implicit T: Monoid[T]): Monoid[T] = T
 
   // ApplicativeSyntax
-  implicit def PapplicativeOpsA[A](a: A): ApplicativeSyntax.OpsA[A] = new ApplicativeSyntax.OpsA(a)
+  implicit def PapplicativeOpsA[A](a: A): ApplicativeSyntax.OpsA[A] =
+    new ApplicativeSyntax.OpsA(a)
 
   // ApplySyntax
-  implicit def PapplyOps[F[_], A](fa: F[A])(implicit F: Apply[F]): ApplySyntax.Ops[F, A] =
+  implicit def PapplyOps[F[_], A](fa: F[A])(
+      implicit F: Apply[F]): ApplySyntax.Ops[F, A] =
     new ApplySyntax.Ops(fa)
 
   // BindSyntax
-  implicit def PbindOps[M[_], A](ma: M[A])(implicit M: Bind[M]): BindSyntax.Ops[M, A] =
+  implicit def PbindOps[M[_], A](ma: M[A])(
+      implicit M: Bind[M]): BindSyntax.Ops[M, A] =
     new BindSyntax.Ops(ma)
 
   // ComposeSyntax
-  implicit def PcomposeOps[P[_, _], A, B](pab: P[A, B])(implicit P: Compose[P]): ComposeSyntax.Ops[P, A, B] =
+  implicit def PcomposeOps[P[_, _], A, B](pab: P[A, B])(
+      implicit P: Compose[P]): ComposeSyntax.Ops[P, A, B] =
     new ComposeSyntax.Ops(pab)
 
   // FunctorSyntax
-  implicit def PfunctorOps[F[_], A](fa: F[A])(implicit F: Functor[F]): FunctorSyntax.Ops[F, A] =
+  implicit def PfunctorOps[F[_], A](fa: F[A])(
+      implicit F: Functor[F]): FunctorSyntax.Ops[F, A] =
     new FunctorSyntax.Ops(fa)
 
-  implicit def CobindOps[F[_], A](fa: F[A])(implicit F: Cobind[F]): CobindSyntax.Ops[F, A] =
+  implicit def CobindOps[F[_], A](fa: F[A])(
+      implicit F: Cobind[F]): CobindSyntax.Ops[F, A] =
     new CobindSyntax.Ops(fa)
 
-  implicit def ComonadOps[F[_], A](fa: F[A])(implicit F: Comonad[F]): ComonadSyntax.Ops[F, A] =
+  implicit def ComonadOps[F[_], A](fa: F[A])(
+      implicit F: Comonad[F]): ComonadSyntax.Ops[F, A] =
     new ComonadSyntax.Ops(fa)
 
   implicit def ShowOps[A](a: A)(implicit A: Show[A]): ShowSyntax.Ops[A] =
     new ShowSyntax.Ops[A](a)(A)
 
-  implicit def SemigroupOps[A](a: A)(implicit A: Semigroup[A]): SemigroupSyntax.OpsA[A] =
+  implicit def SemigroupOps[A](a: A)(
+      implicit A: Semigroup[A]): SemigroupSyntax.OpsA[A] =
     new SemigroupSyntax.OpsA[A](a)
-
-
   // Base Data
   // =========
 
