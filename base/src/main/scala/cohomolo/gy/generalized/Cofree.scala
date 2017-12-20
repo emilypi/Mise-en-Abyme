04f01c977139cc35b624f9d2960ee432005140cb
Emily Pillmore <emilyp@cakesolutions.net>
2017-12-20 14:49:29 -0800

    cofree fixes

--- base/src/main/scala/cohomolo/gy/generalized/Cofree.scala
+++ base/src/main/scala/cohomolo/gy/generalized/Cofree.scala
@@ -1,18 +1,16 @@
 package cohomolo.gy
 package generalized
 
-import cohomolo.gy.prelude.leibniz.Maybe2
-
 trait CofreeModule {
   type Cofree[F[_], A]
 
   def runCofree[F[_], A](
-      f: Cofree[F, A]): Maybe2[A, F[generalized.Cofree[F, A]]]
+      f: Cofree[F, A]): F[generalized.Cofree[F, A]]
 }
 
 private[generalized] object CofreeImpl extends CofreeModule {
-  type Cofree[F[_], A] = Maybe2[A, F[generalized.Cofree[F, A]]]
+  type Cofree[F[_], A] = (A, F[generalized.Cofree[F, A]])
 
   def runCofree[F[_], A](
-      f: Cofree[F, A]): Maybe2[A, F[generalized.Cofree[F, A]]] = f
+      f: Cofree[F, A]): F[generalized.Cofree[F, A]] = f._2
 }
