20809bd73da9c7ff4744405d4f0b796837bb9e08
Emily Pillmore <emilyp@cakesolutions.net>
2017-12-21 15:45:33 -0800

    cofree with empty case

--- base/src/main/scala/cohomolo/gy/generalized/Cofree.scala
+++ base/src/main/scala/cohomolo/gy/generalized/Cofree.scala
@@ -9,31 +9,34 @@ import cohomolo.gy.prelude.typeclass.Functor
 trait CofreeModule {
   type Cofree[F[_], A]
 
-  def mapCofree[F[_]: Functor, A, B](
-      f: A => B,
-      g: generalized.Cofree[F, A] => generalized.Cofree[F, B],
-      cofree: Cofree[F, A]
+  def empty[F[_], A]: Cofree[F, A]
+
+  def mapCofree[F[_]: Functor, A, B](c: Cofree[F, A])(f: A => B)(
+      g: generalized.Cofree[F, A] => generalized.Cofree[F, B]
   ): Cofree[F, B]
 
   def runCofree[F[_], A](f: => Cofree[F, A]): Inf[F[generalized.Cofree[F, A]]]
-  def <::[F[_], A](a: A, f: => F[generalized.Cofree[F, A]]): Cofree[F, A]
+
+  /**
+    * Alias to <::
+    */
+  def ccons[F[_], A](a: A, f: => F[generalized.Cofree[F, A]]): Cofree[F, A]
 }
 
 private[generalized] object CofreeImpl extends CofreeModule {
   type Cofree[F[_], A] = Maybe2[A, Inf[F[generalized.Cofree[F, A]]]]
 
-  def mapCofree[F[_]: Functor, A, B](
-      f: A => B,
-      g: generalized.Cofree[F, A] => generalized.Cofree[F, B],
-      cofree: Cofree[F, A]
-  ): Cofree[F, B] = {
-    lazy val ff = Just2.unapply[A, Inf[F[generalized.Cofree[F, A]]]](cofree)
-    Maybe2.just2(f(ff._1), Inf.apply(Functor[F].map(ff._2.force)(g)))
+  def empty[F[_], A] = Maybe2.empty2[A, Inf[F[generalized.Cofree[F, A]]]]
+
+  def mapCofree[F[_]: Functor, A, B](c: Cofree[F, A])(f: A => B)(
+      g: generalized.Cofree[F, A] => generalized.Cofree[F, B]): Cofree[F, B] = {
+    lazy val ff = Just2.unapply[A, Inf[F[generalized.Cofree[F, A]]]](c)
+    ccons(f(ff._1), Functor[F].map(ff._2.force)(g))
   }
 
   def runCofree[F[_], A](f: => Cofree[F, A]): Inf[F[generalized.Cofree[F, A]]] =
     Just2.unapply[A, Inf[F[generalized.Cofree[F, A]]]](f)._2
 
-  def <::[F[_], A](a: A, f: => F[generalized.Cofree[F, A]]): Cofree[F, A] =
+  def ccons[F[_], A](a: A, f: => F[generalized.Cofree[F, A]]): Cofree[F, A] =
     Maybe2.just2(a, Inf.apply(f))
 }
