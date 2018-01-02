102c4de23a732e734ae482fd996ef884c8c07653
Emily Pillmore <emilyp@cakesolutions.net>
2018-01-01 21:22:14 -0500

    cofree updates

--- base/src/main/scala/cohomolo/gy/generalized/Cofree.scala
+++ base/src/main/scala/cohomolo/gy/generalized/Cofree.scala
@@ -6,7 +6,7 @@ import cohomolo.gy.prelude.control.{Coinductive, Inf}
 sealed abstract class CofreeFunctions {
   type Cofree[F[_], A]
 
-  def runCofree[F[_], A](f: => Cofree[F, A]): F[generalized.Cofree[F, A]]
+  def runCofree[F[_], A](f: Cofree[F, A]): F[generalized.Cofree[F, A]]
 
   def wrapCofree[F[_], A](a: A)(f: => F[generalized.Cofree[F, A]]): Cofree[F, A]
 }
@@ -14,7 +14,7 @@ sealed abstract class CofreeFunctions {
 private[generalized] object CofreeImpl extends CofreeFunctions {
   type Cofree[F[_], A] = (A, Coinductive[F[generalized.Cofree[F, A]]])
 
-  def runCofree[F[_], A](f: => Cofree[F, A]): F[generalized.Cofree[F, A]] =
+  def runCofree[F[_], A](f: Cofree[F, A]): F[generalized.Cofree[F, A]] =
     f._2.force
 
   def wrapCofree[F[_], A](a: A)(
--- base/src/main/scala/cohomolo/gy/generalized/IStream.scala
+++ base/src/main/scala/cohomolo/gy/generalized/IStream.scala
@@ -1,35 +1,19 @@
 package cohomolo.gy
 package generalized
 
-import cohomolo.gy.prelude.control.{Coinductive, Inf}
-import cohomolo.gy.prelude.leibniz.Maybe2
-
 sealed abstract class IStreamModule {
   type IStream[A]
 
-  def empty[A]: IStream[A]
-  def scons[A](a: => A)(as: => generalized.IStream[A]): IStream[A]
+  def scons[A](a: => A)(as: => IStream[A]): IStream[A]
   def uncons[A](as: IStream[A]): A
-  def smap[A, B](as: => IStream[A])(f: A => B)(
-      g: generalized.IStream[A] => generalized.IStream[B]): IStream[B]
 }
 
 private[generalized] object IStreamImpl extends IStreamModule {
-  type IStream[A] = Maybe2[A, Coinductive[generalized.IStream[A]]]
+  type IStream[A] = Cofree[(() => A, ?), ()]
 
-  def empty[A]: IStream[A] =
-    Maybe2.empty2[A, Coinductive[generalized.IStream[A]]]
+  def scons[A](a: => A)(as: => IStream[A]): IStream[A] =
+    Cofree.wrapCofree[(() => A, ?), ()](())((() => a, as))
 
-  def scons[A](a: => A)(as: => generalized.IStream[A]): IStream[A] =
-    Maybe2.just2(a, Coinductive.apply(Inf.apply(as)))
-
-  def uncons[A](as: IStream[A]): A =
-    Maybe2.Just2.unapply[A, Coinductive[generalized.IStream[A]]](as)._1
-
-  def smap[A, B](as: => IStream[A])(f: A => B)(
-      g: generalized.IStream[A] => generalized.IStream[B]): IStream[B] = {
-    lazy val lr =
-      Maybe2.Just2.unapply[A, Coinductive[generalized.IStream[A]]](as)
-    Maybe2.just2(f(lr._1), Coinductive.apply(Inf.apply(g(lr._2.force))))
-  }
+  def uncons[A](as: Cofree[(() => A, ?), ()]): A =
+    Cofree.runCofree(as)._1()
 }
